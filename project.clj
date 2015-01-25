890d87a8242aba18a93ff749630a5345bb5f596e
Steve Miner <steveminer@gmail.com>
2015-01-25 15:33:38 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velisco/tagged "0.3.6-SNAPSHOT"
+(defproject com.velisco/tagged "0.3.6"
   :description "Clojure library for printing and reading Records as EDN tagged literals"
   :url "http://github.com/miner/tagged"
   :license {:name "Eclipse Public License"
